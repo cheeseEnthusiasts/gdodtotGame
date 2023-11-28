1a1d5c442c37b0f48c19c71e617a69f7a89ca45e
“Echo” <echorw927@gmail.com>
2023-11-27 19:22:32 -0800

    move to gdcript

--- Scripts/movement.gd
+++ Scripts/movement.gd
@@ -2,10 +2,15 @@ extends Node
 
 
 # Called when the node enters the scene tree for the first time.
+var speed = 1.0
+
 func _ready():
 	pass # Replace with function body.
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	var velocity = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
