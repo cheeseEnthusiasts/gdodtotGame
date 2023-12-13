007cbd32dfc87b2844a858f71d37be7978b40d8f
WanderingMonument <Dingospoodle1@gmail.com>
2023-12-12 16:54:45 -0800

    Fixed a lot of stuff :)
    yay

--- Scripts/Wizzard/wizzardMovement.cs
+++ Scripts/Wizzard/wizzardMovement.cs
@@ -1,11 +1,13 @@
 using Godot;
 using System;
+using System.Data.Common;
 
 public partial class wizzardMovement : CharacterBody2D
 {
 	public const float Speed = 300.0f;
-	public bool isFacingRight;
+	public bool isFacingRight = true;
 	public Node2D characterNode;
+	Camera2D cameraNode;
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
@@ -15,25 +17,30 @@ public partial class wizzardMovement : CharacterBody2D
 		{
 			velocity.X = direction.X * Speed;
 			velocity.Y = direction.Y * Speed;
-			
+
 		}
 		else
 		{
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
 			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
 		}
+
 		
-	
-		MoveAndSlide();
-		if (characterNode.Scale.X > 0) 
+
+		if (direction.X < 0 && isFacingRight)
 		{
+			ApplyScale(new Vector2(-1,1));
+			isFacingRight = false;
+		}
+
+		if(direction.X > 0 && !isFacingRight)
+		{
+			ApplyScale(new Vector2(-1,1));
 			isFacingRight = true;
 		}
 
-
-		if(isFacingRight && Input.IsKeyPressed(Key.E))
-		{
-			ApplyScale(new Vector2(-1,1));
-		}
+		Velocity = velocity;
+		MoveAndSlide();
+		
 	}
 }
