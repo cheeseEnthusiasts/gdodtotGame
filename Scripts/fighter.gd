f0449dd2b6ae6a78d0740be45326560cd03af8ea
“Echo” <echorw927@gmail.com>
2023-12-18 18:08:35 -0800

    minor main room edits

--- Scripts/fighter.gd
+++ Scripts/fighter.gd
@@ -7,7 +7,7 @@ func _physics_process(delta):
 
 func fighter_movement(delta):
 	
-	if Input.is_action_pressesd("ui_right"):
+	if Input.is_action_pressesd("right"):
 		velocity.x = speed
 		velocity.y = 0
 		
